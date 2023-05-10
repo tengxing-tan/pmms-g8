c11591dbc8c2ecec008aca2e28763fefda4e67f9
Tan Teng Xing <tantengxing@gmail.com>
2023-05-10 08:59:39 +0800

    jetsrtream: change logo & color

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('inventory.index') }}" :active="request()->routeIs('inventory.index')">
+                        {{ __('Inventory') }}
+                    </x-nav-link>
                 </div>
             </div>
 
