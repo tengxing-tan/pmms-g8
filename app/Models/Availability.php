d349e3d717d91fa6286147bc5bef28466cfd1cd3
Jibran-7 <133353001+Jibran-7@users.noreply.github.com>
2023-12-24 18:01:33 +0800

    Create Availability.php

---
+++ app/Models/Availability.php
@@ -0,0 +1,8 @@
+class Availability extends Model
+{
+    protected $fillable = ['user_id', 'available_date', 'start_time', 'end_time'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
